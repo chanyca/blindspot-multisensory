b4d73bf84d3040d679fb4ac29a43b12b68a9514f
chanyca <chanyca@caltech.edu>
2025-06-13 17:15:32 -0700

    Update to include flash resp location hits

--- Data/mat2csv.m
+++ Data/mat2csv.m
@@ -12,6 +12,11 @@
 %     - Blind spot: no 0, yes 1
 %     - Response
 %     - Response type: flash 1, beep 2
+%     - loc_1: hit for location 1
+%     - loc_2: hit for location 2
+%     - loc_3: hit for location 3
+%     - loc_4: hit for location 4
+%     - loc_5: hit for location 5
 
 matFiles = dir('SV*.mat');
 
@@ -24,7 +29,7 @@ for i = 1:length(matFiles)
     subj_id = Data.SubjectID; % Assuming the subject ID is stored in Data.SubjectID
     eye = Data.Eye; % Assuming eye information is stored in Data.Eye
     
-    
+    num_trials = size(Data.Conditions, 1);
     trial_no = 1:size(Data.Conditions,1); 
 
     location_trials = [Data.Conditions(:,1)];
@@ -59,17 +64,42 @@ for i = 1:length(matFiles)
     n_beep = [Data.Conditions(:,2)];
     response = [Data.ResponsesF, Data.ResponsesB];
     response_type = [Data.Conditions(:,3)];
+
+    % Get location hits
+    flash_loc = Data.Flash_loc;
+
+    loc_1 = zeros(num_trials, 1);
+    loc_2 = zeros(num_trials, 1);
+    loc_3 = zeros(num_trials, 1);
+    loc_4 = zeros(num_trials, 1);
+    loc_5 = zeros(num_trials, 1);
+
+    for t = 1:size(flash_loc, 2)
+        current_locs = flash_loc{t};
+        if isscalar(current_locs)
+            % Single location (e.g., 4)
+            eval(['loc_' num2str(current_locs) '(t) = 1;']);
+        else
+            % Multiple locations (e.g., [3,5])
+            for loc = current_locs
+                eval(['loc_' num2str(loc) '(t) = 1;']);
+            end
+        end
+    end
     
     % Create a table
-    T = table(trial_no', n_flash, n_beep, direction, blindspot,  response', response_type, ...
-              'VariableNames', {'trial_no', 'n_flash', 'n_beep', 'direction', 'blindspot', 'response', 'response_type'});
+    T = table(trial_no', n_flash, n_beep, direction, blindspot,  ...
+        response', response_type, loc_1, loc_2, loc_3, loc_4, loc_5, ...
+        'VariableNames', {'trial_no', 'n_flash', 'n_beep', 'direction', 'blindspot', ...
+        'response', 'response_type', 'loc_1', 'loc_2', 'loc_3', 'loc_4', 'loc_5'});
     
     % Add subject ID and eye as additional columns
     T.subj_id = repmat({subj_id}, height(T), 1);
     T.eye = repmat({eye}, height(T), 1);
     
     % Rearrange columns
-    T = T(:, {'subj_id', 'eye', 'trial_no', 'n_flash', 'n_beep', 'direction', 'blindspot', 'response', 'response_type'});
+    T = T(:, {'subj_id', 'eye', 'trial_no', 'n_flash', 'n_beep', 'direction', 'blindspot', ...
+        'response', 'response_type', 'loc_1', 'loc_2', 'loc_3', 'loc_4', 'loc_5'});
     
     % Write to a CSV file
     outputFileName = sprintf('csv/%s_%s.csv', subj_id, eye);
